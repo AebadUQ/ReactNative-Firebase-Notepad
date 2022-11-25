87619ff9a2b2afcbf637398d320fea22ffb8bbad
AebadUQ <aebadulquadir123@gmail.com>
2022-11-25 18:01:37 +0500

    "Crud ka R"

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {View,Text,TextInput,StyleSheet,StatusBar,Dimensions,TouchableOpacity} from 'react-native';
+import {View,Text,TextInput,StyleSheet,StatusBar,Dimensions,FlatList, TouchableOpacity} from 'react-native';
 import React , {useEffect, useState} from 'react';
 import firestore from '@react-native-firebase/firestore';
 import database from '@react-native-firebase/database';
@@ -6,36 +6,37 @@ const {height,width} = Dimensions.get("screen")
 export default function App(){
   const [note,setNote]=useState(null)
   const [list,setList]=useState(null)
-  // const [myData,setMyData]=useState(null)
-  // const [myDataTwo,setMyDataTwo]=useState(null)
-  // useEffect(()=>{
-  //   getDatabase(  )
-  // },[])
-  // const getDatabase  =async()=>{
-  //   try{
-      
-  //     const userDocument =await firestore().collection('testing').doc('GJmk1ztSx2Wbveo2C2xK').get();
-  //     const data =await database().ref('users/1').once("value")
-  //     console.log(data?.val())
-  //     console.log(userDocument?._data)
-  //     setMyData(userDocument?._data)
-  //   }
-  //   catch(err){
-  //     console.log(err)
-  //   } 
-  // }
+  const [myData,setMyData]=useState(null)
+  const [myDataTwo,setMyDataTwo]=useState(null)
+  useEffect(()=>{
+    getDatabase()
+  },[])
+  const getDatabase  =async()=>{
+    try{
+          const data =await database().ref('notes').on("value",(tempData)=>{
+        setList(tempData.val()) 
+      })
+
+    }
+    catch(err){
+      console.log(err)
+    } 
+  }
   const handleAddData=async()=>{
     try{
-      const response =await database().ref("notes/2").set([
+      const index=list.length
+      console.log("index",index)
+      const response =await database().ref(`notes/${index}`).set([
         note
       ]);
       console.log(response)
+      setNote(null)
     }
     catch(err){
       console.log("error=>",err)
     } 
   }
-  console.log(note)
+  console.log(list)
   return(
     <View style={styles.container}>
       <StatusBar hidden={true}/>
@@ -47,6 +48,25 @@ export default function App(){
           <Text style={{color:'white'}}>Add</Text>
      </TouchableOpacity>
      </View>
+
+     <View style={styles.cardCont}>
+     <FlatList 
+         data={list}
+         renderItem={(item)=>{
+         console.log(item)
+          if(item.item !== null)
+         {
+          console.log(item.item[0])
+          return(
+            <View style={styles.card}>
+              <Text style={{color:'black',height:30}}>{item.item[0]}</Text>
+              </View>
+          )
+         }
+         }}
+         />
+     
+    </View>
       {/* <Text>
       {myData ? myData?.name : "loading"}
       </Text>
@@ -76,6 +96,18 @@ const styles=StyleSheet.create(
       backgroundColor:'orange',
       alignItems:'center',
       padding:10
+    },
+    cardCont:{
+      marginVertical:20,
+
+    },
+    card:{
+      backgroundColor:'white',
+      width:width-40,
+      height:30,
+      padding:20,
+      borderRadius:30,
+      marginVertical:10
     }
   }
 )
